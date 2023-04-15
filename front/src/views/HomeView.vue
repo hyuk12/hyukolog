8246894ccc72f3d233a37e988ed88ba449c09e5d
hyuk12 <hotman7025@naver.com>
2023-04-15 22:17:38 +0900

    front vue 완성 초기단계

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,34 +2,80 @@
 
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 const posts = ref([]);
 
-axios.get('/api/posts?page=1&size=5').then((response) => {
+axios.get(`/api/posts?page=1&size=5`).then((response) => {
     response.data.forEach((res: any) => {
         posts.value.push(res);
     });
 });
+
 </script>
 
 <template>
     <ul>
-        <li v-for="post in posts" key="post.id">
-            <div>
-              {{ post.title }}
+        <li v-for="post in posts" :key="post.id">
+            <div class="title">
+              <router-link :to="{name: 'read', params: {postId: post.id}}">{{ post.title }}</router-link>
             </div>
 
-            <div>
+            <div class="content">
                 {{ post.content }}
+            </div>
+
+            <div class="sub d-flex">
+                <div class="category">개발</div>
+                <div class="regDate">2023-04-15 22:16:45</div>
             </div>
         </li>
     </ul>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+ul {
+    list-style: none;
+    padding: 0;
 
-li {
-    margin-bottom: 1rem;
+    li {
+        margin-bottom: 2rem;
+
+        .title {
+            a{
+                font-size: 1.1rem;
+                color: #383838;
+                text-decoration: none;
+            }
+
+            &:hover {
+                a {
+                    text-decoration: underline;
+                }
+            }
+        }
+
+        .content {
+            font-size: 0.85rem;
+            margin-top: 8px;
+            color: #7e7e7e;
+        }
+
+        &:last-child {
+            margin-bottom: 0;
+        }
+
+        .sub {
+            margin-top: 8px;
+            font-size: 0.78rem;
+
+            .regDate {
+                margin-left: 10px;
+                color: #6b6b6b;
+            }
+        }
+    }
 }
-
 </style>
